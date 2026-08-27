9caa6c1276afee10bf2fc9dc102a679ed4764530
saniu <saniulnaim@gmail.com>
2026-08-27 18:57:04 +0600

    Add generic repository

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>(); // AddScoped > It's scoped to the lifetime of the Http request.
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+  public interface IGenericRepository<T> where T : BaseEntity
+  {
+    Task<T?> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<bool> SaveAllAsync();
+    bool Exists(int id);
+  }
+}
